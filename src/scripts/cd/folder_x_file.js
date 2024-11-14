e653c8557ebf3fac75ce5b286c826bf9b5768038
bennykok <itechbenny@gmail.com>
2024-11-14 15:32:33 +0900

    fix: backward support file uploads + models dropdown

---
+++ src/scripts/cd/folder_x_file.js
@@ -0,0 +1,328 @@
+// NOTE: the file anmes in the first array of each value are by default not used, you have to change 
+// the call to api-rewrite.js::updateFoldersFiles to keep them.
+export const folders_files = {
+  "vae": [
+    [
+    ],
+    {
+      "/public_models/vae": 1719107431.6698074,
+      "/comfyui/models/vae": 1719767526.1018212,
+      "/private_models/vae": 1719212768.982683,
+      "/public_models/vae/Stable-Cascade": 1719107431.6698074
+    },
+    17.112636835
+  ],
+  "clip": [
+    [
+      "Stable-Cascade/model.safetensors"
+    ],
+    {
+      "/public_models/clip": 1708245695.7654173,
+      "/comfyui/models/clip": 1719767526.0988212,
+      "/public_models/clip/Stable-Cascade": 1708245695.7654173
+    },
+    115.276156284
+  ],
+  "onnx": [
+    [],
+    {
+      "/comfyui/models/onnx": 1719767654.718759
+    },
+    116.074617749
+  ],
+  "sams": [
+    [
+      "sam_vit_b_01ec64.pth"
+    ],
+    {
+      "/public_models/sams": 1709527802.18717,
+      "/comfyui/models/sams": 1719767652.7257462
+    },
+    115.363894359
+  ],
+  "unet": [
+    [
+      "IC-Light/iclight_sd15_fc.safetensors",
+      "Stable-Cascade/stage_b.safetensors",
+      "Stable-Cascade/stage_c.safetensors",
+      "iclight_sd15_fbc_unet_ldm.safetensors"
+    ],
+    {
+      "/public_models/unet": 1719019581.815384,
+      "/comfyui/models/unet": 1719767526.1018212,
+      "/public_models/unet/IC-Light": 1715877595.4810693,
+      "/public_models/unet/Stable-Cascade": 1708245685.0915735
+    },
+    115.277172375
+  ],
+  "loras": [
+    [
+    ],
+    {
+      "/public_models/loras": 1716140393.973611,
+      "/comfyui/models/loras": 1719767526.100821,
+      "/private_models/loras": 1719791864.870205,
+      "/comfyui/models/photomaker": 1719767526.100821,
+      "/public_models/loras/ipadapter": 1709527589.7121482,
+      "/public_models/loras/SDXL-Lightning": 1709424720.711543
+    },
+    115.275378993
+  ],
+  "pulid": [
+    [],
+    {},
+    116.079953634
+  ],
+  "gligen": [
+    [],
+    {
+      "/comfyui/models/gligen": 1719767526.100821
+    },
+    115.280630489
+  ],
+  "configs": [
+    [
+      "anything_v3.yaml",
+      "v1-inference.yaml",
+      "v1-inference_clip_skip_2.yaml",
+      "v1-inference_clip_skip_2_fp16.yaml",
+      "v1-inference_fp16.yaml",
+      "v1-inpainting-inference.yaml",
+      "v2-inference-v.yaml",
+      "v2-inference-v_fp32.yaml",
+      "v2-inference.yaml",
+      "v2-inference_fp32.yaml",
+      "v2-inpainting-inference.yaml"
+    ],
+    {
+      "/comfyui/models/configs": 1719767526.099821
+    },
+    115.281190841
+  ],
+  "inpaint": [
+    [
+      "MAT_Places512_G_fp16.safetensors"
+    ],
+    {
+      "/public_models/inpaint": 1709698815.797055
+    },
+    115.358945962
+  ],
+  "instantid": [
+    [],
+    {},
+    115.325489542
+  ],
+  "ipadapter": [
+    [
+      "ip-adapter-faceid-plus_sd15.bin",
+      "ip-adapter-faceid-plusv2_sd15.bin",
+      "ip-adapter-faceid-plusv2_sdxl.bin",
+      "ip-adapter-faceid-portrait-v11_sd15.bin",
+      "ip-adapter-faceid-portrait_sdxl.bin",
+      "ip-adapter-faceid-portrait_sdxl_unnorm.bin",
+      "ip-adapter-faceid_sd15.bin",
+      "ip-adapter-faceid_sdxl.bin",
+      "ip-adapter-plus-face_sdxl_vit-h.safetensors",
+      "ip-adapter-plus_sd15.safetensors",
+      "ip-adapter_sdxl.safetensors",
+      "ip-adapter_sdxl_vit-h.safetensors"
+    ],
+    {
+      "/public_models/ipadapter": 1716239161.2595801
+    },
+    115.302328079
+  ],
+  "controlnet": [
+    [
+      "CN-anytest_v3-50000_pn_dim256.safetensors",
+      "SD1.5/animatediff/v3_sd15_sparsectrl_rgb.ckpt",
+      "SDXL/TTPLANET_Controlnet_Tile_realistic_v2_fp16.safetensors",
+      "control-lora-canny-rank256.safetensors",
+      "depth-zoe-xl-v1.0-controlnet.safetensors",
+      "diffusers_xl_canny_full.safetensors",
+      "diffusers_xl_depth_full.safetensors"
+    ],
+    {
+      "/public_models/controlnet": 1719849349.47478,
+      "/comfyui/models/controlnet": 1719767526.099821,
+      "/private_models/controlnet": 1719425899.410212,
+      "/public_models/controlnet/SDXL": 1719814021.600114,
+      "/public_models/controlnet/SD1.5": 1716239205.9015498,
+      "/public_models/controlnet/SD1.5/animatediff": 1716239205.9015498
+    },
+    22.251975897
+  ],
+  "embeddings": [
+    [],
+    {
+      "/comfyui/models/embeddings": 1719767526.100821
+    },
+    115.321684869
+  ],
+  "photomaker": [
+    [],
+    {
+      "/comfyui/models/photomaker": 1719767526.100821
+    },
+    115.284792912
+  ],
+  "vae_approx": [
+    [
+      "taesd3_decoder.pth"
+    ],
+    {
+      "/public_models/vae_approx": 1719085538.701429,
+      "/comfyui/models/vae_approx": 1719767526.1018212
+    },
+    115.272135286
+  ],
+  "checkpoints": [
+    [
+    ],
+    {
+      "/public_models/checkpoints": 1719519280.642045,
+      "/comfyui/models/checkpoints": 1719767526.0988212,
+      "/private_models/checkpoints": 1719425854.3782172,
+      "/public_models/checkpoints/SVD": 1713301325.4918423,
+      "/public_models/checkpoints/upscale": 1712290124.814295,
+      "/private_models/checkpoints/upscale": 1719425854.3782172,
+      "/private_models/checkpoints/SDXL-TURBO": 1718496546.2487228
+    },
+    17.111375401
+  ],
+  "clip_vision": [
+    [
+      "CLIP-ViT-H-14-laion2B-s32B-b79K.safetensors",
+      "CLIP-ViT-bigG-14-laion2B-39B-b160k.safetensors",
+      "clip_vision_g.safetensors",
+      "model.safetensors"
+    ],
+    {
+      "/public_models/clip_vision": 1716186460.546641,
+      "/comfyui/models/clip_vision": 1719767526.0988212
+    },
+    115.27975112
+  ],
+  "style_models": [
+    [],
+    {
+      "/comfyui/models/style_models": 1719767526.100821
+    },
+    115.279050883
+  ],
+  "hypernetworks": [
+    [],
+    {
+      "/comfyui/models/hypernetworks": 1719767526.100821
+    },
+    115.283089474
+  ],
+  "kjnodes_fonts": [
+    [
+      "FreeMono.ttf",
+      "FreeMonoBoldOblique.otf",
+      "TTNorms-Black.otf"
+    ],
+    {
+      "/comfyui/custom_nodes/ComfyUI-KJNodes/fonts": 1719857418.7983456
+    },
+    115.298266863
+  ],
+  "saved_prompts": [
+    [],
+    {},
+    116.085818844
+  ],
+  "upscale_models": [
+    [
+      "4x-UltraSharp.pth",
+      "RealESRGAN_x4.pth"
+    ],
+    {
+      "/public_models/upscale_models": 1710742077.7664135,
+      "/comfyui/models/upscale_models": 1719767526.1018212
+    },
+    19.827387912
+  ],
+  "intristic_loras": [
+    [
+      "intrinsic_lora_sd15_albedo.safetensors",
+      "intrinsic_lora_sd15_depth.safetensors",
+      "intrinsic_lora_sd15_normal.safetensors",
+      "intrinsic_lora_sd15_shading.safetensors",
+      "intrinsic_loras.txt"
+    ],
+    {
+      "/comfyui/custom_nodes/ComfyUI-KJNodes/intristic_loras": 1719857418.9883447
+    },
+    115.300376934
+  ],
+  "ultralytics_bbox": [
+    [
+      "face_yolov8m.pt",
+      "face_yolov8n.pt",
+      "hand_yolov8s.pt"
+    ],
+    {
+      "/public_models/ultralytics/bbox": 1709595164.6940365,
+      "/comfyui/models/ultralytics/bbox": 1719767649.310724
+    },
+    115.36288035
+  ],
+  "ultralytics_segm": [
+    [
+      "person_yolov8m-seg.pt"
+    ],
+    {
+      "/comfyui/models/ultralytics/segm": 1719767649.5477257
+    },
+    115.363269576
+  ],
+  "VHS_video_formats": [
+    [
+      "16bit-png.json",
+      "8bit-png.json",
+      "ProRes.json",
+      "av1-webm.json",
+      "ffmpeg-gif.json",
+      "gifski.json",
+      "h264-mp4.json",
+      "h265-mp4.json",
+      "nvenc_h264-mp4.json",
+      "nvenc_hevc-mp4.json",
+      "webm.json"
+    ],
+    {
+      "/comfyui/custom_nodes/ComfyUI-VideoHelperSuite/videohelpersuite/../video_formats": 1719811040.0935996
+    },
+    115.288357516
+  ],
+  "animatediff_models": [
+    [
+      "hotshotxl_mm_v1.pth",
+      "mm_sd_v14.ckpt",
+      "v3_sd15_mm.ckpt"
+    ],
+    {
+      "/public_models/animatediff_models": 1719813922.626963,
+      "/comfyui/models/animatediff_models": 1719857646.2333913,
+      "/comfyui/custom_nodes/ComfyUI-AnimateDiff-Evolved/models": 1719857272.2370968
+    },
+    19.619891633
+  ],
+  "animatediff_motion_lora": [
+    [],
+    {
+      "/public_models/animatediff_motion_lora": 1716219141.9549487,
+      "/comfyui/models/animatediff_motion_lora": 1719857646.2343915,
+      "/comfyui/custom_nodes/ComfyUI-AnimateDiff-Evolved/motion_lora": 1719857272.2370968
+    },
+    115.326127861
+  ],
+  "input": [
+    [],
+    {},
+    115.288357516
+  ]
+};
